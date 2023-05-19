d04231fa2b62903ee9d74f1684525336a5275f83
isaac boateng <isboateng@hotmail.com>
2023-05-19 17:11:21 +0100

    add templated ang project

--- src/ss-management-dashboard-ui-app/src/app/app.module.ts
+++ src/ss-management-dashboard-ui-app/src/app/app.module.ts
@@ -1,20 +1,27 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
+  ],
   declarations: [
     AppComponent,
-    AuthComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AdminLayoutComponent,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/ss-management-dashboard-ui-app/src/app/guards/auth-permission.guard.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@angular/core";
-
-@Injectable({ providedIn: 'root' })
-export class AuthPermissionsService {
-  isAdmin(isAdmin: boolean) {
-    return isAdmin;
-  }
-}
