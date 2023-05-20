c4577f03db4a156fc46833049407f42882826607
isaac boateng <isboateng@hotmail.com>
2023-05-20 21:26:22 +0100

    added login form, working.

--- src/ss-management-dashboard-ui-app/src/app/login/login.component.ts
+++ src/ss-management-dashboard-ui-app/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoginService } from 'app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
+
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
   ngOnInit() {
+  }
+
+  submit() {
+    const email = this.loginForm.get('email').value;    
+    const passwd = this.loginForm.get('password').value;
+    this.loginService.login(email, passwd);
   }
 
 }
