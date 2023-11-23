08f51f7b60ce121a72687b2744556808107aa0a9
isaac boateng <isboateng@hotmail.com>
2023-11-23 18:12:28 +0000

    update

--- src/ss-management-dashboard-ui-app/src/app/screen-list/screen-list.component.ts
+++ src/ss-management-dashboard-ui-app/src/app/screen-list/screen-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ScreenModel } from 'app/models/screen-response.model';
 import { DataService } from 'app/services/data.service';
 import { AuthService } from 'app/services/auth.service';
+import { NotificationsService } from 'app/notifications';
 
 @Component({
   selector: 'app-screen-list',
@@ -12,52 +13,67 @@ export class ScreenListComponent implements OnInit {
 
   listData: ScreenModel[] = null;
 
-  constructor(private dataService: DataService, private authService: AuthService) { }
+  constructor(private dataService: DataService, private authService: AuthService, private notification: NotificationsService) { }
 
   ngOnInit() {
     this.fetchListData();
   }
 
-  fetchListData(){
+  fetchListData() {
     this.dataService.fetchScreens().subscribe(
       {
         next: (data) => this.listData = data,
         error: (e) => {
-          if(e.status == 401) 
-          {
+          if (e.status == 401) {
             this.authService.redirectToLogin(true);
           }
-          else
-          {
+          else {
             console.log(e)
           }
         },
         complete: () => console.info('complete')
       });
- }
+  }
 
- deleteScreen(id: string)
- {
-  this.dataService.deleteScreen(id).subscribe(
-    {
-      next: () => 
+  previewScreen() {
+  }
+
+  publishScreen(id: string) {
+    this.dataService.publishScreen(id).subscribe(
       {
-        this.listData.forEach((value,index)=>{
-          if(value.id==id) this.listData.splice(index,1);
+        next: () => {
+          this.notification.showSuccess("PUBLISHED..")
+        },
+        error: (e) => {
+          if (e.status == 401) {
+            this.authService.redirectToLogin(true);
+          }
+          else {
+            console.log(e)
+          }
+        },
+        complete: () => console.info('complete')
       });
-      },
-      error: (e) => {
-        if(e.status == 401) 
-        {
-          this.authService.redirectToLogin(true);
-        }
-        else
-        {
-          console.log(e)
-        }
-      },
-      complete: () => console.info('complete')
-    });
- }
+  }
+
+  deleteScreen(id: string) {
+    this.dataService.deleteScreen(id).subscribe(
+      {
+        next: () => {
+          this.listData.forEach((value, index) => {
+            if (value.id == id) this.listData.splice(index, 1);
+          });
+        },
+        error: (e) => {
+          if (e.status == 401) {
+            this.authService.redirectToLogin(true);
+          }
+          else {
+            console.log(e)
+          }
+        },
+        complete: () => console.info('complete')
+      });
+  }
 
 }
